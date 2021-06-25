3eca4cf25bb1c0808e83bfe913074932a85c1894
dghooker1983 <dhooker@sky.com>
2021-06-25 21:51:42 +0100

    Interface and generic repository added.

---
+++ DataStatsMVC/Models/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using DataStatsMVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DataStatsMVC.Models.Repositories
+{
+    public abstract class GenericRepository<T>
+        : IRepository<T> where T : class
+    {
+        protected ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>()
+                .AsQueryable()
+                .Where(predicate)
+                .ToList();
+        }
+
+        public T Get(int id)
+        {
+            return _context.Find<T>(id);
+                
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public abstract IEnumerable<T> GetCalls();
+
+        public abstract IEnumerable<T> GetCallsByDate(DateTime start, DateTime end);
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public T Update(T entity)
+        {
+            return _context.Update(entity).Entity;
+        }
+    }
+}
---
+++ DataStatsMVC/Models/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DataStatsMVC.Models.Repositories
+{
+    public interface IRepository<T> 
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> GetCalls();
+        IEnumerable<T> GetCallsByDate(DateTime start, DateTime end);
+        void SaveChanges();
+        T Update(T entity);
+    }
+}
